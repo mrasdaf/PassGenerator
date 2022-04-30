76ea18158c3e56de8409d64c37a841ed8f9c7a6e
mrasdaf <104437646+mrasdaf@users.noreply.github.com>
2022-04-30 19:58:57 +0500

    Версия 1.1.0

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import filedialog as fd
 import random
+import webbrowser
 from tkinter import scrolledtext
 
 randomsymbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 onePassLen = 1
 
 window = Tk()
-window.title("Генератор паролей by mrasdaf")
+window.title("Password Generator by mrasdaf")
 window.geometry("475x675")
 window.config(bg="black")
 window.resizable(width=False, height=False)
 
 
 
-countOfPasswordsLabel = Label(window, text="Кол-во паролей:", bg="black", fg="white", font="Arial 14")
-countOfPasswordsLabel.place(x=50, y=50)
+countOfPasswordsLabel = Label(window, text="Кол-во паролей:", bg="black", fg="white", font=("Comic Sans MS", 14))
+countOfPasswordsLabel.place(x=30, y=50)
 
-countOfPasswordsEntry = Entry(window, font="Arial 14")
+countOfPasswordsEntry = Entry(window, font=("Comic Sans MS", 14), bg="grey", fg="white")
 countOfPasswordsEntry.place(x=200, y=50)
 
-onePassLenEntry = Entry(window, font="Arial 14")
+onePassLenEntry = Entry(window, font=("Comic Sans MS", 14), bg="grey", fg="white")
 onePassLenEntry.place(x=200, y=100)
 
 onePassLenLabel = Label(window, text="Кол-во символов в 1 пароле:", bg="black", fg="white", font="Arial 9")
@@ -42,12 +44,11 @@ def generateButtonClick():
         resultText.insert(1.0, "Не удалось распознать число символов в 1 пароле!")
         return False
     i = 1
-    # print("213")
     outputStr = ""
     res = ""
     outputStrFULL = ""
     while i <= cp:
-        res = res + "\n\n"
+        res = res + "\n"
         for u in range(onePassLen):
             res = res + random.choice(randomsymbols)
         outputStrFULL = outputStr + res + "\n"
@@ -57,21 +58,45 @@ def generateButtonClick():
     
 def aboutButtonClick():
     messagebox.showinfo("О программе", """
-    Эта программа позволяет вам генерировать пароли.
-    Программа выбирает случайные символы из последовательности знаков и после чего выводит результат.
+Эта программа позволяет вам генерировать пароли.
+Программа выбирает случайные символы из последовательности знаков и после чего выводит результат.
     
-    by mrasdaf
-    Написано на Python
+by mrasdaf
+Написано на Python
     """)
+    
+def githubclick():
+    webbrowser.open_new_tab('https:\\github.com\mrasdaf\PassGenerator')
+def saveresult():
+    try:
+        Files = (
+                ("Текстовые файлы блокнота", "*.txt"),
+                ("Все файлы", "*.*")
+                )
+        filename = fd.asksaveasfilename(
+            filetypes = Files,
+            defaultextension = Files
+            )
+        with open(filename, "w") as f:
+            s = resultText.get(1.0, END)
+            f.write(s)
+            f.close()
+    except:
+        return False
+generateButton = Button(window, text="Сгенерировать", width=58, command=generateButtonClick, bg="grey", fg="white")
+generateButton.place(x=31, y=150)
 
-generateButton = Button(window, text="Сгенерировать", width=55, command=generateButtonClick)
-generateButton.place(x=38, y=150)
-
-resultText = scrolledtext.ScrolledText(width=50, height=27)
+resultText = scrolledtext.ScrolledText(width=60, height=25, font=("Comic Sans MS", 9), bg="grey", fg="white")
 resultText.place(x=25, y=200)
 
-aboutButton = Button(window, text="О программе", width=15, command=aboutButtonClick)
+aboutButton = Button(window, text="О программе", width=15, command=aboutButtonClick, font=("Comic Sans MS", 9), bg="grey", fg="white")
 aboutButton.place(x=350, y=643)
+
+githublink = Button(window, text="Github", width=15, command=githubclick, font=("Comic Sans MS", 9), bg="grey", fg="white")
+githublink.place(x=220, y=643)
+
+githublink = Button(window, text="Сохранить результат", width=20, command=saveresult, font=("Comic Sans MS", 9), bg="grey", fg="white")
+githublink.place(x=60, y=643)
 
 
 window.mainloop()
